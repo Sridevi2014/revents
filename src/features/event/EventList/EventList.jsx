7e88f07dc6b0c54314165927e9a7ebb5aa71a0d9
Sridevi2014 <sridevips@hotmail.com>
2019-03-31 20:46:56 -0600

    Forms and building a crud app in react

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
-import EventListItems from './EventListItems'
+import React, { Component } from "react";
+import EventListItems from "./EventListItems";
 
- class EventList extends Component {
+class EventList extends Component {
   render() {
-    const {events} = this.props;
-    return ( 
-        <div>
+    const { events, onEventOpen, deleteEvent } = this.props;
+    return (
+      <div>
         <h2>Event List </h2>
-        {events.map((event) =>(
-          <EventListItems key={event.id} event={event}/>
+        {events.map(event => (
+          <EventListItems
+            key={event.id}
+            event={event}
+            onEventOpen={onEventOpen}
+            deleteEvent={deleteEvent}
+          />
         ))}
-        
-       
-        </div>
-        
-    )
+      </div>
+    );
   }
 }
-export default EventList 
+export default EventList;
--- src/features/event/EventList/EventListItems.jsx
+++ src/features/event/EventList/EventListItems.jsx
@@ -1,49 +1,53 @@
-import React, { Component } from 'react';
-import { Segment, Item, List, Button, Icon} from 'semantic-ui-react';
-import EventListAttendee from './EventListAttendee';
+import React, { Component } from "react";
+import { Segment, Item, List, Button, Icon } from "semantic-ui-react";
+import EventListAttendee from "./EventListAttendee";
 
- class EventListItems extends Component {
+class EventListItems extends Component {
   render() {
-    const {event} = this.props;
+    const { event, onEventOpen, deleteEvent } = this.props;
     return (
-        <Segment.Group>
-            <Segment>
-              <Item.Group>
-                <Item>
-                  <Item.Image size="tiny" 
-                  circular 
-                  src={event.hostPhotoURL} />
-                  <Item.Content>
-                    <Item.Header as="a">{event.Title}</Item.Header>
-                    <Item.Description>
-                      Hosted by <a>{event.hostedBY}</a>
-                    </Item.Description>
-                  </Item.Content>
-                </Item>
-              </Item.Group>
-            </Segment>
-            <Segment>
-              <span>
-                <Icon name="clock" /> {event.date} |
-                <Icon name="marker" /> {event.venue}
-              </span>
-            </Segment>
-            <Segment secondary>
-              <List horizontal>
-
-              {event.attendees.map((attendee) =>(
-          <EventListAttendee key={attendee.id} attendee={attendee}/>
-        ))}
-
-              </List>
-            </Segment>
-            <Segment clearing>
-            <span>{event.description}</span>
-              <Button as="a" color="teal" floated="right" content="View" />
-            </Segment>
-          </Segment.Group>
-    )
+      <Segment.Group>
+        <Segment>
+          <Item.Group>
+            <Item>
+              <Item.Image size="tiny" circular src={event.hostPhotoURL} />
+              <Item.Content>
+                <Item.Header as="a">{event.title}</Item.Header>
+                <Item.Description>
+                  Hosted by <a>{event.hostedBy}</a>
+                </Item.Description>
+              </Item.Content>
+            </Item>
+          </Item.Group>
+        </Segment>
+        <Segment>
+          <span>
+            <Icon name="clock" /> {event.date}|
+            <Icon name="marker" /> {event.venue}
+          </span>
+        </Segment>
+        <Segment secondary>
+          <List horizontal>
+            {event.attendees &&
+              event.attendees.map(attendee => (
+                <EventListAttendee key={attendee.id} attendee={attendee} />
+              ))}
+          </List>
+        </Segment>
+        <Segment clearing>
+          <span>{event.description}</span>
+          <Button onClick={deleteEvent(event.id)} as="a" color="red" floated="right" content="Delete" />
+          <Button
+            onClick={onEventOpen(event)}
+            as="a"
+            color="teal"
+            floated="right"
+            content="View"
+          />
+           
+        </Segment>
+      </Segment.Group>
+    );
   }
 }
-export default EventListItems
-
+export default EventListItems;
